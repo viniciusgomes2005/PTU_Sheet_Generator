d8bbf4994a1c6668221f35fb02a2dd2f8201ebba
VInícius Gomes <vinixviii@gmail.com>
2025-12-18 17:40:32 -0300

    firebase integration

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-# main.py
+import firebase_admin
+from firebase_admin import credentials, firestore
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from os import environ as env
 
-# Create a FastAPI "instance"
+load_dotenv()  # carrega o .env
+
+cred_path = env["FIREBASE_SERVICE_KEY"]
+
+if not firebase_admin._apps: 
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 app = FastAPI()
 
-# Define a path operation decorator for the root URL ("/") and a GET operation
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
